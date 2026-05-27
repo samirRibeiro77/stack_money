16d8f5eb1bf6e61b4a1bd995e53d730e8eb78df8
Samir Ribeiro <samir.sjr77@gmail.com>
2026-05-27 11:33:13 -0300

    Theme + Auth + l10n
    Página e estilo aplicados junto com a tradução.

--- lib/features/auth/auth_page.dart
+++ lib/features/auth/auth_page.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:stack_money/core/l10n/app_localizations.dart';
 
-class AuthPage extends StatelessWidget {
-  const AuthPage({super.key});
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context)!;
+
     return Scaffold(
-      appBar: AppBar(title: Text('Stack Money')),
       body: Center(
-        child: ElevatedButton.icon(
-          onPressed: () {},
-          label: Text('Login with Google'),
-          icon: Icon(Icons.login, color: Theme.of(context).colorScheme.secondary,),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              l10n.appName,
+              style: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton.icon(
+              onPressed: () {
+                // Trigger Google Sign In flow (Task 1.1)
+              },
+              icon: const Icon(Icons.login),
+              label: Text(l10n.loginWithGoogle),
+            ),
+          ],
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:stack_money/features/auth/auth_page.dart';
-import 'package:stack_money/theme.dart';
+import 'package:stack_money/core/theme/theme.dart';
+
+import 'core/l10n/app_localizations.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const StackMoneyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class StackMoneyApp extends StatelessWidget {
+  const StackMoneyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Stack Money',
       theme: StackMoneyTheme.themeData,
-      home: const AuthPage(),
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'), // English (Default)
+        Locale('pt'), // Portuguese
+      ],
+      home: const LoginScreen(),
     );
   }
 }
