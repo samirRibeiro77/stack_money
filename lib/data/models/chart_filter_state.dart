36834bb17c5dc9696ce87e120221e8a6285ecac8
Samir Ribeiro <samir.sjr77@gmail.com>
2026-07-06 17:48:49 -0300

    Chart Filter

--- lib/data/models/chart_filter_state.dart
+++ lib/data/models/chart_filter_state.dart
@@ -2,7 +2,7 @@ import 'package:stack_money/data/enum/chart_filter.dart';
 
 class ChartFilterState {
   final ChartFilter filter;
-  final String customLabel; // Guarda o texto "05/09 a 19/11" quando ativo
+  final String customLabel;
   final DateTime? start;
   final DateTime? end;
 
