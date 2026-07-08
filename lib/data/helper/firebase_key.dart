4a2390d53cee89915b0b1f4dceb3d0aad46e9977
Samir Ribeiro <samir.sjr77@gmail.com>
2026-07-08 11:38:07 -0300

    Initial Firebase Keys

---
+++ lib/data/helper/firebase_key.dart
@@ -0,0 +1,8 @@
+class FirebaseKey {
+  /// Data collections
+  static const users = 'users';
+  static const history = 'history';
+  static const buckets = 'buckets';
+  static const salaryPlans = 'salaryPlans';
+  static const netWorth = 'netWorth';
+}
