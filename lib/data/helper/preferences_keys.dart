945dcc7e05a79dca7f00134ac63080b695374ca1
Samir Ribeiro <samir.sjr77@gmail.com>
2026-07-20 18:32:53 -0300

    [Config] Init
    Preferences keys

---
+++ lib/data/helper/preferences_keys.dart
@@ -0,0 +1,7 @@
+class PreferencesKeys {
+  static const isSecureByDefault = 'is_secure_by_default';
+  static const isExpandedDefault = 'is_expanded_default';
+  static const defaultDashboardFilter = 'default_dashboard_filter';
+  static const customUserName = 'custom_user_name';
+  static const customAvatarUrl = 'custom_avatar_url';
+}
