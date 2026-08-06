867bc5eb9268ad5a1859e31064b27ab0f1224593
Samir Ribeiro <samir.sjr77@gmail.com>
2026-08-06 15:47:31 -0300

    [ErrorScree] Remove example of usage

--- lib/features/error/error_screen.dart
+++ lib/features/error/error_screen.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:stack_money/core/constants/app_sizes.dart';
-import 'package:stack_money/core/exceptions/exception_scope.dart';
 import 'package:stack_money/core/exceptions/stack_money_exception.dart';
 import 'package:stack_money/core/helpers/stack_money_string.dart';
 import 'package:stack_money/core/l10n/app_localizations.dart';
-import 'package:stack_money/core/providers/app_coordinator.dart';
 import 'package:stack_money/core/theme/theme.dart';
 import 'package:stack_money/features/error/widgets/error_details.dart';
 import 'package:stack_money/features/error/widgets/error_header.dart';
@@ -21,15 +19,6 @@ class ErrorScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final l10n = AppLocalizations.of(context)!;
 
-    final e = StackMoneyException(
-      message: 'Error in plan timeline stream',
-      scope: ExceptionScope.network,
-      payload: AppCoordinator.instance.user.value.toJson(keepPrefs: true),
-      stackTrace: StackTrace.fromString(
-        AppCoordinator.instance.buckets.value.map((b) => b.toJson()).toString(),
-      ),
-    );
-
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -37,7 +26,7 @@ class ErrorScreen extends StatelessWidget {
           onPressed: () => SystemNavigator.pop(),
         ),
         title: Text(
-          StackMoneyString.formatTitle('${e.scope.name} ${l10n.error}'),
+          StackMoneyString.formatTitle('${exception.scope.name} ${l10n.error}'),
         ),
       ),
       body: Padding(
@@ -48,19 +37,19 @@ class ErrorScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            ErrorHeader(icon: e.scope.icon, message: e.message),
+            ErrorHeader(icon: exception.scope.icon, message: exception.message),
             Expanded(child: SizedBox.shrink()),
-            if (e.payload != null) ...[
+            if (exception.payload != null) ...[
               ErrorDetails(
                 title: l10n.payload,
-                detail: e.payload,
+                detail: exception.payload,
                 color: StackMoneyTheme.cyanNeon,
               ),
             ],
-            if (e.stackTrace != null) ...[
+            if (exception.stackTrace != null) ...[
               ErrorDetails(
                 title: l10n.stackTrace,
-                detail: e.stackTrace,
+                detail: exception.stackTrace,
                 color: StackMoneyTheme.magentaNeon,
               ),
             ],
