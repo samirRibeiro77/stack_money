08adf676df3eea4abba80526ad95821a45d1d598
Samir Ribeiro <samir.sjr77@gmail.com>
2026-08-10 23:44:41 -0300

    [AI] Chat list tab

---
+++ lib/features/chats/chats_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:stack_money/core/constants/app_sizes.dart';
+import 'package:stack_money/core/l10n/app_localizations.dart';
+import 'package:stack_money/core/theme/theme.dart';
+import 'package:stack_money/core/widgets/card_initialize_slot.dart';
+import 'package:stack_money/core/widgets/expandable_header.dart';
+import 'package:stack_money/core/widgets/title_text.dart';
+import 'package:stack_money/features/chats/manager/chats_manager.dart';
+
+class ChatsScreen extends StatefulWidget {
+  const ChatsScreen({super.key = const ValueKey(route)});
+
+  static const route = '/chats';
+
+  @override
+  State<ChatsScreen> createState() => _AiChatsScreenState();
+}
+
+class _AiChatsScreenState extends State<ChatsScreen> {
+  final _manager = ChatsManager();
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context)!;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        ExpandableHeader(
+          title: l10n.plansConfig,
+          validation: _manager.showArchivedNotifier,
+          toggle: _manager.toggleShowArchived,
+          activeIcon: Icons.archive_outlined,
+          inactiveIcon: Icons.unarchive_outlined,
+          activeColor: StackMoneyTheme.magentaNeon,
+          inactiveColor: StackMoneyTheme.cyanNeon,
+        ),
+        const SizedBox(height: AppSizes.sizedBoxMedium),
+        CardInitializeSlot(
+          'New chat',
+          onTap: () => _manager.initializeNewBucketSlot(context),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/features/chats/manager/chats_manager.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class ChatsManager {
+  final ValueNotifier<bool> _showArchived = ValueNotifier(false);
+
+  ValueListenable<bool> get showArchivedNotifier => _showArchived;
+
+  void toggleShowArchived() {
+    _showArchived.value = !_showArchived.value;
+  }
+
+  void initializeNewBucketSlot(BuildContext context) {
+    if (context.mounted) {
+      context.push('unknown'); // TODO: Add CFO Chat route
+    }
+  }
+}
